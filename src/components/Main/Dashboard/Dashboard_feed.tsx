6cab351b37f71efcfc3085289ff6ec4c2704c0f7
sdifeo <s.difeo2@gmail.com>
2024-03-30 05:09:32 -0400

    gettng event information for dashboard

--- src/components/Main/Dashboard/Dashboard_feed.tsx
+++ src/components/Main/Dashboard/Dashboard_feed.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import EventInfo from '../../../models/Event/EventInfo';
 import Dashboard_event from './Dashboard_event';
 import { User } from '../../../models/User/User';
+import GroupEvents from '../../../models/Group/GroupEvents';
 
 interface Dashboard_feedProps {
     currentUser: User | undefined
@@ -13,12 +14,13 @@ interface Dashboard_feedProps {
 const Dashboard_feed: React.FC<Dashboard_feedProps> = ({ currentUser }) => {
 
     const { groupId } = useParams()
-    const [events, setEvents] = useState<EventInfo[]>([]);
+    const [eventIds, setEventIds] = useState<GroupEvents[]>([]);
     const [posts, setPosts] = useState<any>();
 
     useEffect(() => {
         const fetchEventsAndPostsAsync = async () => {
-            await fetchTenLatestEventsCreated();
+            let eventIds = await fetchEventIds();
+            await getEventInformation(eventIds!)
             // await fetchTopTenLatestPostsCreated();
         }
 
@@ -28,7 +30,7 @@ const Dashboard_feed: React.FC<Dashboard_feedProps> = ({ currentUser }) => {
         }
     }, [currentUser])
 
-    const fetchTenLatestEventsCreated = async () => {
+    const fetchEventIds = async () => {
 
         // if user is looking at a specific group, we will get the 10 most recent events OF THAT GROUP
         // if user is NOT looking at a group, we will get the 10 most recent events of ALL their groups
@@ -37,15 +39,15 @@ const Dashboard_feed: React.FC<Dashboard_feedProps> = ({ currentUser }) => {
                 try {
                     const eventsData = await Promise.all(
                         currentUser.groups.map(async (group) => {
-                            const groupDocRef = doc(db, "groups", group);
+                            const groupDocRef = await doc(db, "groups", group);
                             const docSnap = await getDoc(groupDocRef);
-                            return docSnap.exists() ? { ...docSnap.data() } : null;
+                            return await docSnap.exists() ? { ...docSnap.data()!.events } : null;
                         })
                     );
                     // Filter out any null values that may have resulted from missing documents
-                    const validEvents: EventInfo[] = eventsData.filter((event): event is EventInfo => event !== null);
-                    setEvents(validEvents);
-                    console.log(events)
+                    const validEvents: GroupEvents[] = eventsData.filter((data): data is GroupEvents => data !== null);
+                    await setEventIds(validEvents as GroupEvents[]);
+                    return validEvents;
                 } catch (error) {
                     console.error("Error getting group documents:", error);
                 }
@@ -57,8 +59,8 @@ const Dashboard_feed: React.FC<Dashboard_feedProps> = ({ currentUser }) => {
             await getDoc(eventsDocRef)
                 .then((docSnap) => {
                     if (docSnap.exists()) {
-                        setEvents(docSnap.data().events);
-                        console.log("EVENTS: " + events)
+                        setEventIds(docSnap.data().events);
+                        console.log("EVENTS: " + eventIds)
                     } else {
                         console.log("no documents");
                     }
@@ -70,26 +72,46 @@ const Dashboard_feed: React.FC<Dashboard_feedProps> = ({ currentUser }) => {
 
     }
 
-    const fetchTopTenLatestPostsCreated = async () => {
-        const postsQuery = query(collection(db, 'posts'), orderBy('data.created_at'), limit(10));
-        const queryEventsSnapshot = await getDocs(postsQuery);
+    const getEventInformation = async (eventIds: GroupEvents[]) => {
+        if (eventIds.length > 0) {
+            try {
+                const eventsData = await Promise.all(
+                    eventIds.map(async (event) => {
+                        const eventsDocRef = await doc(db, "events", event);
+                        const docSnap = await getDoc(eventsDocRef);
+                        return await docSnap.exists() ? { ...docSnap.data() } : null;
+                    })
+                );
+                // Filter out any null values that may have resulted from missing documents
+                const validEvents: GroupEvents[] = eventsData.filter((data): data is GroupEvents => data !== null);
+                await setEventIds(validEvents as GroupEvents[]);
+                return validEvents;
+            } catch (error) {
+                console.error("Error getting group documents:", error);
+            }
 
-        const posts = queryEventsSnapshot.docs.map(doc => ({
-            id: doc.id,
-            ...doc.data()
-        }));
+        }
 
-        setPosts(posts)
-    }
+        const fetchTopTenLatestPostsCreated = async () => {
+            const postsQuery = query(collection(db, 'posts'), orderBy('data.created_at'), limit(10));
+            const queryEventsSnapshot = await getDocs(postsQuery);
 
-    return (
-        <div className='w-1/2 mx-auto'>
-            {/* {events ? events[0].location : null} */}
-            {/* {events ? events.map((eventInfo, index) => (
+            const posts = queryEventsSnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+
+            setPosts(posts)
+        }
+
+        return (
+            <div className='w-1/2 mx-auto'>
+                {/* {events ? events[0].address : null} */}
+                {/* {events ? events.map((eventInfo, index) => (
                 <Dashboard_event key={index} eventInfo={eventInfo} ></Dashboard_event>
             )) : null} */}
-        </div>
-    )
-}
+            </div>
+        )
+    }
 
-export default Dashboard_feed
+    export default Dashboard_feed
